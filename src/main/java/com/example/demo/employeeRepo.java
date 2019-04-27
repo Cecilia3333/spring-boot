474d22010a699108ecf051e6897664613426b18c
Cecilia3333 <38930809+Cecilia3333@users.noreply.github.com>
2019-04-27 11:43:51 +0800

    Add files via upload

--- src/main/java/com/example/demo/employeeRepo.java
+++ src/main/java/com/example/demo/employeeRepo.java
@@ -1,9 +1,9 @@
 
 package com.example.demo;
- 
+
 import com.example.demo.employeeRepo;
 import org.springframework.data.jpa.repository.JpaRepository;
- 
+
 
 public interface employeeRepo extends JpaRepository<employee, Integer>{
 
